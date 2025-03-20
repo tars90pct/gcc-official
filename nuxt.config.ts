74b66fb621eed17df13776d13c0ca3a7f02f8af0
711openjohn <711openjohn@gmail.com>
2025-03-21 00:35:48 +0900

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  ssr: false,
   icon: {
     customCollections: [
       {
-        prefix: 'gcc',
-        dir: './assets/icons'
+        prefix: "gcc",
+        dir: "./assets/icons",
       },
     ],
-  }
-})
+  },
+});
