d0d9020e0a95b5271f2f227f2d3f6d15422df461
711openjohn <711openjohn@gmail.com>
2025-03-21 00:39:04 +0900

    route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
+  app: {
+    baseURL: "/gcc-official/",
+    buildAssetsDir: "assets",
+  },
   ssr: false,
   icon: {
     customCollections: [
